1e4179c66958041f7b4e726a5820bc7caa6fb432
Hannyyoop <xiaohanny947@gmail.com>
2024-10-01 22:43:36 +0630

    first

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,15 @@
+<div class="navbar mx-auto bg-base-100">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl"><img src="asset('images/logo.png')" class="" alt=""></a>
+    </div>
+    <div class="flex-none ">
+      <ul class="menu menu-horizontal px-1 me-44 text-xl">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Our Location</a></li>
+        <li><a href="#">Join the Franchise</a></li>
+
+      </ul>
+    </div>
+  </div>
